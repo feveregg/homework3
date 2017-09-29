8d3e4ee1c91dca2453a25fa9fedc79a51f0c6f0b
feveregg <wk45@afsconnect2.njit.edu>
2017-09-29 00:11:42 -0400

    1

--- index.php
+++ index.php
@@ -1,5 +1,21 @@
 <?php
+$text = 'my text';
+$obj = new main();
+$obj->printthis($text);
 
-echo "hello world"
+
+class main {
+
+ public function printThis($text) {
+ 
+ echo '<h1>print function demo</h1>';
+ print($text);
+ echo'<hr>';
+ 
+ }
+
+
+}
+
 
 ?>
