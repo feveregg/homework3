31629ba3a978c884b9c801a609ba7abd74534753
feveregg <wk45@afsconnect2.njit.edu>
2017-09-29 00:26:20 -0400

    2

--- index.php
+++ index.php
@@ -2,18 +2,25 @@
 $text = 'my text';
 $obj = new main();
 $obj->printthis($text);
-
+$obj->cslash($text);
 
 class main {
 
- public function printThis($text) {
+   public function printThis($text) {
  
- echo '<h1>print function demo</h1>';
- print($text);
- echo'<hr>';
+   echo '<h1>print function demo</h1>';
+   print($text);
+   echo'<hr>';
  
- }
+   }
 
+   public function cslash($text) {
+
+   echo "<h1>trim function demo</h1>";
+   echo chop($text, "text");
+   echo "<hr>";
+
+   }
 
 }
 
